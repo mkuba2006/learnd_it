82083deca65bb86a473aaa034e2554638f980b22
Jakub Myrta <mkuba2006@gmail.com>
2024-07-06 23:40:28 +0200

    first commit

---
+++ scripts/nav.js
@@ -0,0 +1,30 @@
+const button_flag = document.getElementById('flag');
+const language = document.getElementById('language');
+const flag = document.getElementById('flag_iamge');
+
+button_flag.addEventListener('click', ()=>{
+    if(language.textContent == 'EN'){
+        language.textContent = 'PL';
+        flag.setAttribute('src', './images/flags/poland.png');
+    } else{
+        language.textContent = 'EN';
+        flag.setAttribute('src', './images/flags/uk.png');
+    }
+})
+
+
+
+function toggleMenu() {
+    const burger = document.getElementById('burger');
+    const nav2 = document.getElementById('nav2');
+    const navigation = document.getElementById('navigation');
+
+    burger.classList.toggle('open'); 
+    nav2.classList.toggle('open'); 
+
+    if (burger.classList.contains('open')) {
+        navigation.style.transform = 'translateY(0)';
+    } else {
+        navigation.style.transform = 'translateY(-100vh)'; // Adjust this value as needed
+    }
+}
