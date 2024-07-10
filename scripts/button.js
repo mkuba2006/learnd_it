596965bb667b94d97e45024396b45f5a543272b4
Jakub Myrta <mkuba2006@gmail.com>
2024-07-10 16:24:24 +0200

    10.07

--- scripts/button.js
+++ scripts/button.js
@@ -14,3 +14,22 @@ studentButton.addEventListener('click', () => {
   studentButton.classList.add('active');
   tutorButton.classList.remove('active');
 });
+
+
+
+document.getElementById('student').addEventListener('click', ()=> {
+  scrollToSection('student_info');
+});
+
+document.getElementById('tutor').addEventListener('click', ()=> {
+  scrollToSection('tutor_info');
+});
+
+function scrollToSection(sectionId) {
+  const container = document.getElementById('infos');
+  const section = document.getElementById(sectionId);
+  container.scrollTo({
+    left: section.offsetLeft,
+    behavior: 'smooth'
+  });
+}
