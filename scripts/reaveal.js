bdc51c52f6ff5ddfd6b7e109160b4279a178c9e5
Jakub Myrta <mkuba2006@gmail.com>
2024-07-16 20:47:19 +0200

    x

--- scripts/reaveal.js
+++ scripts/reaveal.js
@@ -1,18 +1,18 @@
-ScrollReveal().reveal('#first_content h2', { delay: 100, origin: 'left' });
-ScrollReveal().reveal('#first_content p', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('#form', { delay: 500, origin: 'left' });
-ScrollReveal().reveal('#colabs img', { delay: 700, interval: 200, origin: 'left' });
-ScrollReveal().reveal("#first_image", { delay: 700, origin: 'bottom' })
+// ScrollReveal().reveal('#first_content h2', { delay: 100, origin: 'left' });
+// ScrollReveal().reveal('#first_content p', { delay: 300, origin: 'left' });
+// ScrollReveal().reveal('#form', { delay: 500, origin: 'left' });
+// ScrollReveal().reveal('#colabs img', { delay: 700, interval: 200, origin: 'left' });
+// ScrollReveal().reveal("#first_image", { delay: 700, origin: 'bottom' })
 
 
-ScrollReveal().reveal('#second #description h1', { delay: 100, origin: 'left' });
-ScrollReveal().reveal('#second #description h2', { delay: 300, origin: 'left' });
-ScrollReveal().reveal('#second #content #el1', { delay: 600, origin: 'left', duration: 400, distance: '60px' });
-ScrollReveal().reveal('#second #content #el3', { delay: 1000, origin: 'left', duration: 400, distance: '60px' });
-ScrollReveal().reveal('#second #content #el2', { delay: 800, origin: 'right', duration: 400, distance: '60px' });
-ScrollReveal().reveal('#second #content #el4', { delay: 1200, origin: 'right', duration: 400, distance: '60px' });
-ScrollReveal().reveal('#second #content #phone', { delay: 400, origin: 'bottom', duration: 400, distance: '60px' });
+// ScrollReveal().reveal('#second #description h1', { delay: 100, origin: 'left' });
+// ScrollReveal().reveal('#second #description h2', { delay: 300, origin: 'left' });
+// ScrollReveal().reveal('#second #content #el1', { delay: 600, origin: 'left', duration: 400, distance: '60px' });
+// ScrollReveal().reveal('#second #content #el3', { delay: 1000, origin: 'left', duration: 400, distance: '60px' });
+// ScrollReveal().reveal('#second #content #el2', { delay: 800, origin: 'right', duration: 400, distance: '60px' });
+// ScrollReveal().reveal('#second #content #el4', { delay: 1200, origin: 'right', duration: 400, distance: '60px' });
+// ScrollReveal().reveal('#second #content #phone', { delay: 400, origin: 'bottom', duration: 400, distance: '60px' });
 
 
-ScrollReveal().reveal('#third h1', { delay: 100, origin: 'left' });
-ScrollReveal().reveal('#third #content #box #lessons', { delay: 200, interval: 100, duration: 1400, origin: 'left', distance: '30px' });
+// ScrollReveal().reveal('#third h1', { delay: 100, origin: 'left' });
+// ScrollReveal().reveal('#third #content #box #lessons', { delay: 200, interval: 100, duration: 1400, origin: 'left', distance: '30px' });
