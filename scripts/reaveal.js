17e529cdf8d69c2dc07736152c9759c5e62169f4
Jakub Myrta <mkuba2006@gmail.com>
2024-07-27 14:44:42 +0200

    x

--- scripts/reaveal.js
+++ scripts/reaveal.js
@@ -4,9 +4,13 @@ const flag22 = document.getElementById('flag2');
 flag22.addEventListener('click', () => {
     if (submit2.value === 'Odbierz Wczesny Dostęp') {
         submit2.style.transform = 'translateX(-100px)';
-        console.log(submit2.value);
-    }else{
+        document.getElementById("right_fourth").style.transform= 'transform: translate(-30px, 353px);'
+        document.getElementById("data_box5").style.transform= 'transform: translate(-30px, 380px);'
+    }
+    else{
         submit2.style.transform = 'translateX(-80px)';
-        submit2.style.padding = ' 0 5px'
+        submit2.style.padding = ' 0 5px';
+        document.getElementById("right_fourth").style.transform= 'transform: translate(-30px, 373px);'
+        document.getElementById("data_box5").style.transform= 'transform: translate(-30px, 404px);'
     }
 });
